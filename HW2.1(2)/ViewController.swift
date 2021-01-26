7d0c519144d06f1aa305e96ec98a66759bded5dc
S.Kornev <post.kornev@gmail.com>
2021-01-27 01:06:54 +0300

    Final release of HW 2.1(2)

--- HW2.1(2)/ViewController.swift
+++ HW2.1(2)/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var trafficLight: UIStackView!
+    
+    @IBOutlet var switchTrafficLightButton: UIButton!
+    
+    private var currentTrafficLightSectionActive = -1 // None active sections
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        switchTrafficLightButton.layer.cornerRadius = 10
+        for singleLight in trafficLight.arrangedSubviews {
+            singleLight.layer.cornerRadius = 75
+        }
     }
 
+    @IBAction func switchLightButtonPressed() {
+        if currentTrafficLightSectionActive == -1 {
+            switchTrafficLightButton.setTitle("Next", for: .normal)
+            currentTrafficLightSectionActive = 0
+            trafficLight.arrangedSubviews[currentTrafficLightSectionActive].alpha = 1
+        } else {
+            trafficLight.arrangedSubviews[currentTrafficLightSectionActive].alpha = 0.3
+            currentTrafficLightSectionActive += 1
+            if currentTrafficLightSectionActive > 2 {
+                currentTrafficLightSectionActive = 0
+            }
+            trafficLight.arrangedSubviews[currentTrafficLightSectionActive].alpha = 1
+        }
 
+    }
+    
 }
 
